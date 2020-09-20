9662a654134cce279084275c238372bacb4c948b
04tushar <tushargoyal04@gmail.com>
2020-09-20 17:13:48 +0530

    content change

---
+++ bookkeeping_and_accounting.php
@@ -0,0 +1,75 @@
+<?php include('header.php');?>
+
+    <section class="action d-flex justify-content-center align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2>Bookkeeping & Accounting </h2>
+                    <!-- <p>Bookkeeping & Accounts</p> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="aboutUs mt-4" data-aos="fade" data-aos-duration="1000" data-aos-delay="20">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="img__bookkeeping_and_accounts"></div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="details__aboutUs">
+                        <p>
+                            Book-Keeping & Accounting is one of the many services offered by Acontax Consultancy to its clients. As a service provider of accounting and bookkeeping services PAN India, we efficiently cater to the varied day-to-day back-office accounting needs of our clients.
+                        </p>
+
+                        <p>
+                            With the changing business environment and the new digitized process of recording and documentation, it is crucial to maintain and review accounting data in line with Indian Accounting Standards and Policies. We ensure hassle-free and compliant management of your “Lekha-Jhokha” with our services over different platforms like Tally ERP9, Zoho, QuickBook as well as on client’s custom software.
+                        </p>
+
+                        <p>
+                            We believe in delivering real-time solutions to Accounting and Book-Keeping issues and provide the best possible solution meeting the demands of our clients. We facilitate all the required services to our clients.
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="aboutUs mt-4" data-aos="fade" data-aos-duration="1000" data-aos-delay="20">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="details__aboutUs">
+                        <p>
+                            <b>We have no limitations, but our core expertise is:</b>
+                        </p>
+
+                        <p>
+                            <ul style="margin-left: 17px;">
+                                <li>
+                                    To provide corporate assistance in the formation of accounting policies & procedures.
+                                </li>
+                                <li>
+                                    To do core analysis and processing the accounting reports and deliver the general ledger bookkeeping.
+                                </li>
+                                <li>
+                                    To maintain the Cash-Flow records of all the sectors of business. Recording Accounts-Payable and Accounts-Receivable with accuracy.
+                                </li>
+                                <li>
+                                    To assist in the valuation of the stock to the clients.
+                                </li>
+                            </ul>
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <section class="services container">
+        
+    </section>
+<?php include('footer.php');?>
